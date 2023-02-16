eb551cce7363362a4b31a8bb2602a5813c8739fa
Michele Mancioppi <michelem@lumigo.io>
2023-02-16 17:15:43 +0100

    fix: maybe it works now?

--- .github/workflows/scripts/set_prerelease_tag.sh
+++ .github/workflows/scripts/set_prerelease_tag.sh
@@ -1,7 +1,4 @@
 # The GitHub Ref is going to be a Pull ref, e.g., `refs/pull/123/head`
 TAG="pr-$(echo \"${GITHUB_REF}\" | awk -F/ '{ print $3 }')-${GITHUB_SHA:0:7}-lumigo"
 
-if [[ $TAG =~ ^v[0-9]+\.[0-9]+\.[0-9]+.* ]]
-then
-    echo "tag=$TAG" >> $GITHUB_OUTPUT
-fi
+echo "tag=$TAG" >> $GITHUB_OUTPUT
