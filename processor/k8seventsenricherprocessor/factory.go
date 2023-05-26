9646708e9e650c752ab648cf4edb97372a99976e
Michele Mancioppi <michelem@lumigo.io>
2023-05-26 16:58:41 +0200

    fix: ponto lint

--- processor/k8seventsenricherprocessor/factory.go
+++ processor/k8seventsenricherprocessor/factory.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package k8seventsenricherprocessor
+package k8seventsenricherprocessor // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/k8seventsenricherprocessor"
 
 import (
 	"context"
